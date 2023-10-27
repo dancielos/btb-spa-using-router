4b613f9c61e68971d339013dae201f34689023df
dancielos <iam@dancielos.com>
2023-10-27 10:25:29 -0600

    new project setup

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = function (props) {
-	return (
-		<>
-			<h1>My Homepage</h1>
-		</>
-	);
-};
-
-export default Home;
--- src/pages/Products.jsx
+++
@@ -1,5 +0,0 @@
-const Products = function (props) {
-	return <h1>Products Page</h1>;
-};
-
-export default Products;
